919c3d7711ef79499be618460b92f9ed6acc51de
lukaszfronia <86720750+lukaszfronia@users.noreply.github.com>
2023-03-26 21:19:31 +0200

    delete download CV button

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -16,14 +16,6 @@ const Hero = () => {
           <p className="text-xl font-bold sm:text-2xl md:text-3xl text-gray-800">
             {hero.description}
           </p>
-          <a
-            href="CV.pdf"
-            download
-            className="flex justify-center items-center gap-4 text-xl font-semibold bg-slate-100 px-8  py-4 rounded-[15px] shadow-lg shadow-gray-500 cursor-pointer  w-[250px] md:w-[200px]"
-          >
-            <img src={hire} className="w-[35px] h-[35px] scale-x-[-1] " />
-            Hire Me !
-          </a>
         </div>
       </div>
     </div>
